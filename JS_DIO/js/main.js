d7f08c2b668102336738d248e161d71bf2f5390d
Caroline Duarte <duarte.carol@gmail.com>
2021-04-08 21:34:35 +0100

    inicio js

---
+++ JS_DIO/js/main.js
@@ -0,0 +1,11 @@
+var nome = "Carol Duarte";
+var idade = 25;
+var idade2 = 10;
+var frase = "Brasil é o país mais bonito do mundo."
+//alert("Bem vinda " + nome + " tem " + idade + " anos.");
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase);
+console.log(frase.replace("mundo","universo"));
+console.log(frase.toUpperCase());
