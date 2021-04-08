af8e47dc9470e8435231aaaeb16f89dcff5f440a
Caroline Duarte <duarte.carol@gmail.com>
2021-04-08 22:12:43 +0100

    mais js

--- JS_DIO/js/main.js
+++ JS_DIO/js/main.js
@@ -1,4 +1,55 @@
-var nome = "Carol Duarte";
+var d = new Date();
+//alert(d);
+//alert(d.getMonth()+1);
+alert(d.getDate());
+
+/*var count = 0;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};*/
+
+
+/*var count = 0;
+while(count < 5){
+    console.log(count);
+    alert(count);
+    count = count + 1;
+};*/
+
+
+/*var idade = prompt("Qual a sua idade?")
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+
+
+/*var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas[1]);
+console.log(frutas[1].nome);*/
+
+
+
+/*var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta);
+console.log(fruta.cor);*/
+
+/*var lista = ["maçã", "pêra", "laranja"];
+console.log(lista);
+//alert(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+lista.length;
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.toString()[9]);
+console.log(lista.join("|"));*/
+
+/*var nome = "Carol Duarte";
 var idade = 25;
 var idade2 = 10;
 var frase = "Brasil é o país mais bonito do mundo."
@@ -8,4 +59,4 @@ console.log(nome);
 console.log(idade+idade2);
 console.log(frase);
 console.log(frase.replace("mundo","universo"));
-console.log(frase.toUpperCase());
+console.log(frase.toUpperCase());*/
